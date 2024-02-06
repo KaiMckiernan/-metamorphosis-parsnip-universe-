0f7f136ec66282d4a434fdf46bf51ef9d3d91bb8
KaiMckiernan <18kmckiernan@CMS.tela.org.uk>
2024-02-06 22:24:11 +0000

    RAAH

--- metamorphisize.js
+++ metamorphisize.js
@@ -1,37 +1,39 @@
 //Were gonna take a string, and randomly input "Metamorphosis", "Parsnip" and "Universe" into it
 
-console.log(produceMPU(1000,0.5));
+console.log(produceMPU(30,0.8));
 
 function produceMPU(length,sparsity) {
     //Length is how long the paragraph is
-    //Sparsity is based on how many metamorphosis parsnip universes there are its a number under 1. 
+    //Sparsity is based on how many metamorphosis parsnip universes there are its a numhber under 1. 
     let text = "";
     let counter = 0;
-    let mpu = 0;
-    for(let i = 0; i < length; i++) {
+    let straycount = 0;
+    for(let i = 1; i < length; i++) {
         let random = Math.ceil(Math.random()*3);
         if(Math.random() < (sparsity)) {
             text = text.concat(" Metamorphosis Parsnip Universe");
+            counter = 0;
             i += 2;
-            mpu++;
         } else{
             switch (random) {
                 case 1:
                     text = text.concat(" Metamorphosis");
                     counter = 1;
+                    straycount++;
                     break;
                 case 2: 
                     text = text.concat(" Parsnip");
                     counter = (counter === 1) ? 2 : 0;
+                    straycount++;
                     break;
                 case 3:
                     text = text.concat(" Universe");
                     counter = (counter === 2) ? 3 : 0;
+                    straycount++;
                     break;
                 }
         }
-            if(counter === 3) {mpu++}
+            if(counter === 3) {straycount -=3}
     }
-    return [text,mpu];
-
+    return [text,straycount]
 }
